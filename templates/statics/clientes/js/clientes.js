9b7b61e691b6171a52c49e70985ac31aef112616
robsonafsilva <robsonap.silva@gmail.com>
2023-06-12 16:49:32 -0300

    Cadastro de Clientes

---
+++ templates/statics/clientes/js/clientes.js
@@ -0,0 +1,21 @@
+function add_carro(){
+
+  container = document.getElementById("form-carro")
+
+  html = `<br>
+            <div class='row'> 
+              <div class='col-md'>
+                 <input type='text' placeholder='Carro' class='form-control' name='carro'>
+              </div>
+              <div class='col-md'>
+                <input type='text' placeholder='Placa' class='form-control' name='placa'>
+              </div>
+                <div class='col-md'>
+                  <input type='text' placeholder='Ano' class='form-control' name='ano'>
+                </div>
+            </div>`
+
+  container.innerHTML += html
+
+
+}
